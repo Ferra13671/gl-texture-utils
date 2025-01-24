e7c68e806deab4e7b728ebd0a88e811b27934d58
Ferra13671 <ferra13671@gmail.com>
2025-01-24 14:06:58 +0300

    Added TextureStorage

---
+++ src/main/java/com/ferra13671/TextureUtils/TextureStorage.java
@@ -0,0 +1,34 @@
+package com.ferra13671.TextureUtils;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * @author Ferra13671
+ * @LastUpdate 1.2
+ */
+
+public class TextureStorage {
+    private static final Map<String, GLTexture> TEXTURES = new HashMap<>();
+
+    public static boolean addTexture(String key, GLTexture texture) {
+        if (TEXTURES.containsKey(key)) return false;
+        TEXTURES.put(key, texture);
+        return true;
+    }
+
+    public static boolean removeTexture(String key) {
+        if (!TEXTURES.containsKey(key)) return false;
+        TEXTURES.remove(key);
+        return true;
+    }
+
+    public static GLTexture getTexture(String key) {
+        return TEXTURES.get(key);
+    }
+
+    private static GLTexture register(String key, GLTexture texture) {
+        TEXTURES.put(key, texture);
+        return texture;
+    }
+}
