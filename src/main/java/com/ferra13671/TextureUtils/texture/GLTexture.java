6d742901f0ea6d375cd1aeb7d3300274a0c23ddc
Ferra13671 <ferra13671@gmail.com>
2025-08-14 14:17:35 +0300

    Creating subtexture from texture

--- src/main/java/com/ferra13671/TextureUtils/texture/GLTexture.java
+++ src/main/java/com/ferra13671/TextureUtils/texture/GLTexture.java
@@ -5,13 +5,16 @@ import com.ferra13671.TextureUtils.GLTextureSystem;
 import com.ferra13671.TextureUtils.GlTex;
 import com.ferra13671.TextureUtils.builder.GLTextureInfo;
 import org.lwjgl.opengl.GL11;
+import org.lwjgl.opengl.GL30;
 import org.lwjgl.system.MemoryUtil;
+
+import java.util.Random;
 
 import static org.lwjgl.stb.STBImage.nstbi_image_free;
 
 /**
  * @author Ferra13671
- * @LastUpdate 1.6
+ * @LastUpdate 1.6.1
  */
 
 public class GLTexture implements GlTex {
@@ -20,6 +23,10 @@ public class GLTexture implements GlTex {
     protected String name;
     protected int width;
     protected int height;
+
+    protected TextureFiltering filtering;
+    protected TextureWrapping wrapping;
+    protected ColorMode colorMode;
 
     protected GLTexture(String name) {
         this.name = name;
@@ -43,6 +50,7 @@ public class GLTexture implements GlTex {
         controller.texParameter(GL11.GL_TEXTURE_2D, 34049, 0.0F);
         applyFiltering(controller, glTextureInfo.getFiltering());
         applyWrapping(controller, glTextureInfo.getWrapping());
+        this.colorMode = glTextureInfo.getColorMode();
 
         controller.texImage2D(GL11.GL_TEXTURE_2D, 0, glTextureInfo.getColorMode().glId, width, height, 0, glTextureInfo.getColorMode().glId, 5121, null);
         controller.pixelStore(GL11.GL_UNPACK_ROW_LENGTH, 0);
@@ -64,11 +72,55 @@ public class GLTexture implements GlTex {
     private void applyFiltering(GlController controller, TextureFiltering textureFiltering) {
         controller.texParameter(GL11.GL_TEXTURE_2D, GL11.GL_TEXTURE_MAG_FILTER, textureFiltering.id);
         controller.texParameter(GL11.GL_TEXTURE_2D, GL11.GL_TEXTURE_MIN_FILTER, textureFiltering.id);
+        this.filtering = textureFiltering;
     }
 
     private void applyWrapping(GlController controller, TextureWrapping textureWrapping) {
         controller.texParameter(GL11.GL_TEXTURE_2D, GL11.GL_TEXTURE_WRAP_S, textureWrapping.id);
         controller.texParameter(GL11.GL_TEXTURE_2D, GL11.GL_TEXTURE_WRAP_T, textureWrapping.id);
+        this.wrapping = textureWrapping;
+    }
+
+    public GLTexture subTexture(float u1, float v1, float u2, float v2) {
+        GLTexture texture = new GLTexture(name.concat(String.format("_sub_%s", new Random().nextInt())));
+        GlController controller = GLTextureSystem.getGlController();
+        texture.texId = controller.genTexId();
+
+        int fbo = GL30.glGenFramebuffers();
+        GL30.glBindFramebuffer(GL30.GL_FRAMEBUFFER, fbo);
+        GL30.glFramebufferTexture2D(GL30.GL_FRAMEBUFFER, GL30.GL_COLOR_ATTACHMENT0, GL30.GL_TEXTURE_2D, this.texId, 0);
+
+        if (GL30.glCheckFramebufferStatus(GL30.GL_FRAMEBUFFER) != GL30.GL_FRAMEBUFFER_COMPLETE)
+            throw new RuntimeException(String.format("An error occurred while creating FrameBuffer for subtexture '%s'.", texture.getName()));
+
+        texture.width = (int) ((u2 - u1) * width);
+        texture.height = (int) ((v2 - v1) * height);
+
+        controller.bindTexture(texture.texId);
+        controller.texParameter(GL11.GL_TEXTURE_2D, 33085, 0);
+        controller.texParameter(GL11.GL_TEXTURE_2D, 33082, 0);
+        controller.texParameter(GL11.GL_TEXTURE_2D, 33083, 0);
+        controller.texParameter(GL11.GL_TEXTURE_2D, 34049, 0.0F);
+        texture.applyFiltering(controller, filtering);
+        texture.applyWrapping(controller, wrapping);
+        texture.colorMode = colorMode;
+
+        controller.texImage2D(GL11.GL_TEXTURE_2D, 0, texture.colorMode.glId, texture.width, texture.height, 0, texture.colorMode.glId, 5121, null);
+        controller.pixelStore(GL11.GL_UNPACK_ROW_LENGTH, 0);
+        controller.pixelStore(GL11.GL_UNPACK_SKIP_PIXELS, 0);
+        controller.pixelStore(GL11.GL_UNPACK_SKIP_ROWS, 0);
+        controller.pixelStore(GL11.GL_UNPACK_ALIGNMENT, 4);
+
+        GL11.glCopyTexSubImage2D(GL11.GL_TEXTURE_2D, 0,
+                0, 0,
+                (int) (u1 * width), (int) (v1 * height),
+                texture.width, texture.height);
+
+        controller.bindTexture(0);
+        GL30.glBindFramebuffer(GL30.GL_FRAMEBUFFER, 0);
+        GL30.glDeleteFramebuffers(fbo);
+
+        return texture;
     }
 
     /**
